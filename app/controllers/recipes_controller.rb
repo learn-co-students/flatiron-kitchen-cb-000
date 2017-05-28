c2fc4583a8ed9cfc98f5689eec0c4fbd8782bd82
Matt Rhone <matthew.rhone@outlook.com>
2017-05-28 11:22:15 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,43 @@
 class RecipesController < ApplicationController
+  before_action :get_recipe, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :'new'
+    end
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render :'edit'
+    end
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name, ingredient_ids:[])
+    end
+
+    def get_recipe
+      @recipe = Recipe.find(params[:id])
+    end
 end
