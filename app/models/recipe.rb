b8a0f19f30ffb3a2bb8935238579a853cbe9ba2e
Matt Hough <matt@hough.tv>
2017-12-13 07:42:41 +1000

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,12 @@ class Recipe < ActiveRecord::Base
     has_many :ingredients
 
     validates :name, presence: true
+
+    def ingredients_to_dishes
+        self.no_of_ingredients / 2
+    end
+
+    def no_of_ingredients
+        self.ingredients.length
+    end
 end
