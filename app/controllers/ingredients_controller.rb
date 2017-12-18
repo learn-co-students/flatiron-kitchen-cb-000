abac71e1d979e4f9d7c1c2ce79cb70f886c9196c
Jason Vance <jbvance@gmail.com>
2017-12-18 22:22:27 +0000

    complete create ingredients specs

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,38 @@
 class IngredientsController < ApplicationController
+    def new
+       @ingredient = Ingredient.new 
+    end
+    
+    def index
+       @ingredients = Ingredient.all 
+    end
+    
+    def create
+        ingredient = Ingredient.new(ingredient_params)
+    
+        if ingredient.save
+          redirect_to ingredients_path
+        else
+          render :new
+        end
+    end
+    
+    def edit
+       @ingredient = Ingredient.find(params[:id]) 
+    end
+    
+    def update
+        @ingredient = Ingredient.find(params[:id])
+        @ingredient.update(ingredient_params)
+        if @ingredient.save
+          redirect_to ingredients_path
+        else
+          render :new
+        end
+    end
+    
+    private
+        def ingredient_params
+           params.require(:ingredient).permit(:name)
+        end
 end
