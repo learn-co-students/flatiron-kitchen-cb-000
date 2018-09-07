8b34b11294af78f6a0a1446f7e56512c1c595c36
Quinn Dizon <qdizon@gmail.com>
2018-09-07 20:08:51 +0000

    working

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,24 @@
 class IngredientsController < ApplicationController
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    # binding.pry
+    @ingredient = Ingredient.create(ingredient_params)
+    redirect_to @ingredient
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+    # binding.pry
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
 end
